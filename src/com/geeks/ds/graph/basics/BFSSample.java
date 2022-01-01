d00eb27facc2b77ab70bcdbd28914ce7e5b0e952
maddy <maddyraj65@gmail.com>
2022-01-01 08:28:15 +0530

    Graph

--- src/com/geeks/ds/graph/basics/BFSSample.java
+++ src/com/geeks/ds/graph/basics/BFSSample.java
@@ -1,5 +1,19 @@
 package com.geeks.ds.graph.basics;
+import java.io.*;
+import java.util.*;
 
 public class BFSSample {
-
+	private int V;
+	private LinkedList<Integer> adj[];
+	
+	BFSSample(int v){
+		V = v;
+		adj = new LinkedList[v];
+		
+		for(int i=0; i<v; i++) {
+			adj[i] = new LinkedList();
+		}
+	}
+	
+	
 }
--- src/com/geeks/ds/graph/basics/NumberCount.java
+++ src/com/geeks/ds/graph/basics/NumberCount.java
@@ -3,7 +3,7 @@ package com.geeks.ds.graph.basics;
 public class NumberCount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
