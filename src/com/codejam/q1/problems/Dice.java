da1d58a6490b2ed4a60dc5da05769e8374081ff5
maddy <maddyraj65@gmail.com>
2022-04-02 16:20:31 +0530

    Updated

--- src/com/codejam/q1/problems/Dice.java
+++ src/com/codejam/q1/problems/Dice.java
@@ -30,50 +30,13 @@ public class Dice {
 
 	private static int calculateLength(int[] diceArr, int n) {
 		int count = 0;
-		//Arrays.sort(diceArr);
-		quickSort(diceArr,0,n-1);
-		
+		Arrays.sort(diceArr);
 		for(int i=0; i<n; i++) {
 			if(count < diceArr[i]) {
 				count = count+1;
 			}
 		}
 		return count;
-	}
-
-	private static void quickSort(int[] arr, int low, int high) {
-		if (low < high){
-			
-			int pi = partition(arr, low, high);
-			quickSort(arr, low, pi - 1);
-			quickSort(arr, pi + 1, high);
-		}
-		
-	}
-	
-	static int partition(int[] arr, int low, int high){
-		
-		int pivot = arr[high];
-		int i = (low - 1);
-
-		for(int j = low; j <= high - 1; j++)
-		{
-			
-			if (arr[j] < pivot)
-			{
-				i++;
-				swap(arr, i, j);
-			}
-		}
-		swap(arr, i + 1, high);
-		return (i + 1);
-	}
-	
-	static void swap(int[] arr, int i, int j)
-	{
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
 	}
 
 }
